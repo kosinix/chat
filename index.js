295fda275087b5c7ed9d2f4749fb0784a2de00d2
kosinix <kosinix@users.noreply.github.com>
2019-06-09 21:42:55 +0800

    A working room and nickname chat

--- index.js
+++ index.js
@@ -7,19 +7,31 @@ const express = require('express');
 const lodash = require('lodash');
 const nunjucks = require('nunjucks')
 const socketIO = require('socket.io')
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 //// First things first
 //// Save full path of our root app directory and load config and credentials
 global.APP_DIR = path.resolve(__dirname).replace(/\\/g, '/'); // Turn back slash to slash for cross-platform compat
 global.PORT = 3000
+global.BASE_URL = `http://localhost:${PORT}`
 
 //// Create app
 const app = express();
+// Parse http body
+app.use(bodyParser.json());       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+    extended: true
+}));
+
+// Cookies
+app.use(cookieParser());
+
 const server = http.Server(app);
 var io = socketIO(server);
 
 server.listen(PORT, function () {
-    console.log(`Chat listening on port: ${PORT}`);
+    console.log(`Chat listening on: ${BASE_URL}`);
 });
 
 app.use(express.static(`${APP_DIR}/public`))
@@ -36,11 +48,26 @@ let nunjucksEnv = new nunjucks.Environment(loaderFsNunjucks)
 nunjucksEnv.express(app)
 
 app.get('/', function (req, res) {
-    res.render(`room.html`);
+    res.render(`index.html`);
 });
-app.get('/r/:roomId', function (req, res) {
-    let roomId = lodash.get(req, 'params.roomId')
-    res.render(`room.html`);
+app.post('/join', function (req, res) {
+    let randomString = (length, chars) => {
+        var result = '';
+        for (var i = length; i > 0; --i) result += chars[Math.floor(Math.random() * chars.length)];
+        return result;
+    }
+    let uid = lodash.get(req, 'body.uid')
+    var roomId = randomString(8, '123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+    res.redirect(`/r/?room=${roomId}&uid=${uid}`);
+});
+app.get('/r/', function (req, res) {
+    let roomId = lodash.get(req, 'query.room');
+    let uid = lodash.get(req, 'query.uid');
+    let inviteUrl = `${BASE_URL}/r/?room=${roomId}`;
+    res.render(`room.html`, {
+        inviteUrl: inviteUrl,
+        uid: uid
+    });
 });
 
 // Chat namespace
@@ -49,35 +76,45 @@ const chat = io.of('/chat');
 // middleware
 // chat.use((socket, next) => {
 //     console.log(socket)
-//     let token = socket.handshake.query.token;
-//     if ('abc' === token) {
+//     let room = socket.handshake.query.room;
+//     if ('abc' === room) {
 //         return next();
 //     }
-//     console.log('authentication error', token)
+//     console.log('authentication error', room)
 //     return next(new Error('authentication error'));
 // });
-
 chat.on('connect', function (socket) {
-    let room = socket.handshake.query.token;
+    let room = socket.handshake.query.room;
+    let uid = socket.handshake.query.uid;
     socket.join(room);
-    
+    console.log(`User "${uid}" connected.`)
+
+    // to all clients in room except sender
+    socket.to(room).emit('someone joined', `User "${uid}" has joined the room.`, uid);
+    // to sender only
+    socket.emit('someone joined', 'You joined this room.', uid);
+
     io.of('/chat').clients((error, clients) => {
         if (error) throw error;
-        console.log(clients.length + ' user(s) connected to /chat'); // => [PZDoMHjiu8PYfRiKAAAF, Anw2LatarvGVVXEIAAAD]
+        // console.log(clients.length + ' user(s) connected to /chat'); // => [PZDoMHjiu8PYfRiKAAAF, Anw2LatarvGVVXEIAAAD]
     });
 
     io.of('/chat').in(room).clients((error, clients) => {
         if (error) throw error;
-        console.log(clients.length + ' user(s) connected to '+room); // => [PZDoMHjiu8PYfRiKAAAF, Anw2LatarvGVVXEIAAAD]
+        
+        console.log(`${clients.length} user(s) connected to room "${room}"`); // => [PZDoMHjiu8PYfRiKAAAF, Anw2LatarvGVVXEIAAAD]
     });
 
     socket.on('chat send', function (msg) {
-        chat.to(room).emit('chat received', msg + ' room - '+room);
+        // to all clients in room except sender
+        socket.to(room).emit('chat received', msg, uid);
+        // to sender only
+        socket.emit('sender message', msg, uid);
     });
     socket.on('error', (error) => {
         console.log(error);
     });
     socket.on('disconnect', function () {
-        console.log('user disconnected');
+        console.log(`User "${uid}" disconnected.`)
     });
 });
--- public/js/main.js
+++ public/js/main.js
@@ -1,24 +1,32 @@
 const urlParams = new URLSearchParams(window.location.search);
 const room = urlParams.get('room');
-var socket = io('/chat', { query: "token="+room });
+const uid = urlParams.get('uid');
+var socket = io('/chat', {
+    query: {
+        room: room,
+        uid: uid
+    }
+});
 var app = new Vue({
     el: '#app',
     delimiters: ["${", "}"],
     data: {
         messages: [
-            {
-                content: ''
-            }
+            // {
+            //     author: '',
+            //     content: 'Chat started',
+            //     className: 'item notif'
+            // },
         ],
         message: ''
     },
-    created: function(){
+    created: function () {
         // var urlParams = new URLSearchParams(window.location.search);
         // var room = urlParams.get('room');
         // socket.emit('join room', room);
     },
     methods: {
-        send: function(){
+        send: function () {
             var me = this;
             socket.emit('chat send', me.message);
             me.message = '';
@@ -30,8 +38,27 @@ var app = new Vue({
 //       token: 'fgh'
 //     }
 //   });
-socket.on('chat received', function (msg) {
+socket.on('someone joined', function (msg) {
     app.messages.push({
-        content: msg
+        author: '',
+        content: msg,
+        className: 'item notif'
     })
+});
+socket.on('sender message', function (msg, author) {
+    app.messages.push({
+        author: author,
+        content: msg,
+        className: 'item sender'
+    })
+    window.scrollTo(0,document.body.scrollHeight);
+});
+socket.on('chat received', function (msg, author) {
+    app.messages.push({
+        author: author,
+        content: msg,
+        className: 'item'
+    })
+    // window.scrollTo(0,document.querySelector(".thread").scrollHeight+100);
+
 });
